3874ff8a1756c991d595dc00941ca44879c72b76
Oyadier Robert Mensah <oyadierobertmensah@gmail.com>
2023-04-25 16:29:49 +0100

    Update add_function

--- add_func.c
+++ add_func.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <unistd.h>
 /**
  * _memcpy - copyies n bytes from memory area src to
  *	     the buffer contained in a buffer_t struct.
@@ -113,3 +114,53 @@ unsigned int convert_sbase(buffer_n *output, long int num, char *base,
 	return (ret);
 }
 
+/**
+ * convert_ubase - Converts an unsigned long to an inputted base and
+ *                 stores the result to a buffer contained in a struct.
+ * @output: A buffer_n struct containing a character array.
+ * @num: An unsigned long to be converted.
+ * @base: A pointer to a string containing the base to convert to.
+ * @flags: Flag modifiers.
+ * @wid: A width modifier.
+ * @prec: A precision modifier.
+ *
+ * Return: The number of bytes stored to the buffer.
+ */
+unsigned int convert_ubase(buffer_n *output, unsigned long int num, char *base,
+		unsigned char flags, char wid, char prec)
+{
+	unsigned int size, ret = 1;
+	char digit, pad = '0', *lead = "0x";
+
+	for (size = 0; *(base + size);)
+		size++;
+
+	if (num >= size)
+		ret += convert_ubase(output, num / size, base,
+				flags, wid - 1, prec - 1);
+
+	else
+	{
+		if (((flags >> 5) & 1) == 1)
+		{
+			wid -= 2;
+			prec -= 2;
+		}
+		for (; prec > 1; prec--, wid--)
+			ret += _memcpy(output, &pad, 1);
+
+		if (NEG_FLAG == 0)
+		{
+			pad = (ZERO_FLAG == 1) ? '0' : ' ';
+			for (; wid > 1; wid--)
+				ret += _memcpy(output, &pad, 1);
+		}
+		if (((flags >> 5) & 1) == 1)
+			ret += _memcpy(output, lead, 2);
+	}
+
+	digit = base[(num % size)];
+	_memcpy(output, &digit, 1);
+
+	return (ret);
+}
